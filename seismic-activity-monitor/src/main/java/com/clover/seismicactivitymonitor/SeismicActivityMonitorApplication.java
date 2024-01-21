92a345c39c11c5392c2f9d9aa6fc14034bd42b91
Altrocore <shepelev.daniel.ca@gmail.com>
2024-01-21 11:56:11 -0330

    Initial commit

---
+++ seismic-activity-monitor/src/main/java/com/clover/seismicactivitymonitor/SeismicActivityMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.clover.seismicactivitymonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeismicActivityMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeismicActivityMonitorApplication.class, args);
+	}
+
+}
---
+++ seismic-activity-monitor/src/test/java/com/clover/seismicactivitymonitor/SeismicActivityMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clover.seismicactivitymonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeismicActivityMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
